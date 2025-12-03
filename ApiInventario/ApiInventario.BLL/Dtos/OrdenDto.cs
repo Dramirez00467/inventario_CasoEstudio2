c907ad7a27f093e33b350ddae63ba04f9e0e7440
Dramirez00467 <106043587+Dramirez00467@users.noreply.github.com>
2025-12-03 02:09:30 -0600

    Api Inventario completa
    En ella está la api de productos y la api de ordenes

---
+++ ApiInventario/ApiInventario.BLL/Dtos/OrdenDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiInventario.BLL.Dtos
+{
+    public class OrdenDto
+    {
+        public int Id { get; set; }
+
+        public int ProductoId { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string Estado { get; set; }
+    }
+}
---
+++ ApiInventario/ApiInventario.DLL/Repositorio/IOrdenesRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApiInventario.DLL.Entidades;
+
+namespace ApiInventario.DLL.Repositorio
+{
+    public interface IOrdenesRepositorio
+    {
+        Task<List<Orden>> ObtenerOrdenesAsync();
+        Task<Orden> ObtenerOrdenPorIdAsync(int id);
+        Task<bool> AgregarOrdenAsync(Orden orden);
+        Task<bool> CancelarOrdenAsync(int id);
+    }
+}
